da8bebef70b57ceafdf8ddc4ada50734696d81e4
xy-wu <xy-wu@outlook.com>
2023-12-29 03:02:50 +0900

    add opengl dataio

---
+++ dataIO/data_io.hpp
@@ -0,0 +1,31 @@
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+// std
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+namespace dataIO{
+
+    void initVEF(Eigen::MatrixXd &V, Eigen::MatrixXd &E, Eigen::MatrixXi &F);
+
+    void readObj(std::string filename, Eigen::MatrixXd &V, Eigen::MatrixXd &E, Eigen::MatrixXi &F);
+    
+    void writeObj(std::string filename, Eigen::MatrixXd &V, Eigen::MatrixXd &E, Eigen::MatrixXi &F);
+
+    Eigen::SparseMatrix<int> makeAdjacencyMatrix(Eigen::MatrixXd &V, Eigen::MatrixXd &E, Eigen::MatrixXi &F);
+
+    void fitInputDataStructure(Eigen::MatrixXd &V,
+                      Eigen::MatrixXd &E,
+                      Eigen::MatrixXi &F,
+                      std::vector<Eigen::Vector3d> &cube,
+                      std::vector<std::pair<int, int>> &edges);
+    
+    void fitOutputDataStructure(Eigen::MatrixXd &V,
+                      Eigen::MatrixXd &E,
+                      Eigen::MatrixXi &F,
+                      std::vector<Eigen::Vector3d> &cube,
+                      std::vector<std::pair<int, int>> &edges);
+}
