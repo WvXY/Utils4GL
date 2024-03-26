0a1343df76865212daa349a5318636eb2df84f30
xy-wu <wxy@g.ecc.u-tokyo.ac.jp>
2024-03-26 17:26:46 +0900

    add alias

---
+++ src/global_alias.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_GLOBAL_ALIAS_H
+#define UTILS_GLOBAL_ALIAS_H
+
+#include <Eigen/Dense>
+
+typedef Eigen::Vector2f vec2;
+typedef Eigen::Vector3f vec3;
+typedef Eigen::Vector4f vec4;
+typedef Eigen::Matrix2f mat2;
+typedef Eigen::Matrix3f mat3;
+typedef Eigen::Matrix4f mat4;
+
+typedef  Eigen::Vector3i vec3i;
+
+#endif // UTILS_GLOBAL_ALIAS_H
