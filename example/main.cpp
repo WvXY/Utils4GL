69e91af1b12f7451a94346f7360f49b291dc55cc
xy-wu <wu.xuanyu@hotmail.com>
2024-08-05 11:05:42 +0900

    Small rename

--- example/main.cpp
+++ example/main.cpp
@@ -10,40 +10,40 @@
 #include <windows.h>
 
 int main() {
-  std::vector<vec3> vertices = {{0.0f, 0.5f, 0.f},
-                                {0.5f, -0.5f, 0.f},
-                                {-0.5f, -0.5f, 0.f},
-                                {0.2f, 0.4f, 0.f}};
-  std::vector<vec3> colors = {{1.0f, 1.0f, 0.0f},
-                              {0.0f, 1.0f, 1.0f},
-                              {0.0f, 0.0f, 1.0f},
-                              {1.0f, 0.0f, 1.0f}};
-  std::vector<vec3i> indices = {{0, 1, 2}, {0, 1, 3}};
-  std::vector<vec2> texCoords = {
+  std::vector<vec3> v0 = {{0.0f, 0.5f, 0.f},
+                          {0.5f, -0.5f, 0.f},
+                          {-0.5f, -0.5f, 0.f},
+                          {0.2f, 0.4f, 0.f}};
+  std::vector<vec3> c0 = {{1.0f, 1.0f, 0.0f},
+                          {0.0f, 1.0f, 1.0f},
+                          {0.0f, 0.0f, 1.0f},
+                          {1.0f, 0.0f, 1.0f}};
+  std::vector<vec3i> i0 = {{0, 1, 2}, {0, 1, 3}};
+  std::vector<vec2> tc0 = {
       {0.0f, 0.0f}, {1.0f, 0.0f}, {0.5f, 1.0f}, {0.5f, 0.5f}};
-  auto go = std::make_unique<wvxy::GameObject>(vertices, colors, indices);
+  auto go = std::make_unique<wvxy::GameObject>(v0, c0, i0);
 
-  std::vector<vec3> vertices2 = {{0.0f, 0.8f, 0.f},
-                                 {0.5f, -0.4f, 0.f},
-                                 {0.5f, 0.5f, 0.f},
-                                 {0.2f, 0.4f, 0.f}};
-  std::vector<vec3> colors2 = {{1.0f, 0.0f, 0.0f},
-                               {0.0f, 1.0f, 0.0f},
-                               {0.0f, 0.0f, 1.0f},
-                               {1.0f, 0.0f, 1.0f}};
-  std::vector<vec3i> indices2 = {{0, 1, 2}};
+  std::vector<vec3> v1 = {{0.0f, 0.8f, 0.f},
+                          {0.5f, -0.4f, 0.f},
+                          {0.5f, 0.5f, 0.f},
+                          {0.2f, 0.4f, 0.f}};
+  std::vector<vec3> c1 = {{1.0f, 0.0f, 0.0f},
+                          {0.0f, 1.0f, 0.0f},
+                          {0.0f, 0.0f, 1.0f},
+                          {1.0f, 0.0f, 1.0f}};
+  std::vector<vec3i> i1 = {{0, 1, 2}};
 
   // initialize
   auto time_start{std::chrono::high_resolution_clock::now()};
   auto t0 = time_start;
   size_t frame_count{0};
 
-  wvxy::GlUtils app{800, 800, "Triangle"};
-  auto* window = app.window;
+  wvxy::GlUtils glApp{800, 800, "Triangle"};
+  auto* window = glApp.window;
 
   // TODO add functions to manage all the texture stuff
-  auto tex0 = app.loadTexture("../../assets/wall.jpg");
-  glUniform1i(glGetUniformLocation(app.basicShader.getID(), "tex0"), 0);
+  auto tex0 = glApp.loadTexture("../../assets/wall.jpg");
+  glUniform1i(glGetUniformLocation(glApp.basicShader.getID(), "tex0"), 0);
 
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.4f, 0.6f, 0.1f, 1.f);
@@ -52,8 +52,8 @@ int main() {
     glActiveTexture(GL_TEXTURE);
     glBindTexture(GL_TEXTURE_2D, tex0);
 
-    app.draw(vertices, colors, indices, texCoords);
-    app.draw(vertices2, colors2, indices2);
+    glApp.draw(v0, c0, i0, tc0);
+    glApp.draw(v1, c1, i1);
 
     glfwPollEvents();
     glfwSwapBuffers(window);
@@ -65,7 +65,8 @@ int main() {
       auto t1 = std::chrono::high_resolution_clock ::now();
       std::chrono::duration<double> frame_time = t1 - t0;
       t0 = t1;
-      app.addInfoToTitle("FPS: " + std::to_string(100.f / frame_time.count()));
+      glApp.addInfoToTitle("FPS: " +
+                           std::to_string(100.f / frame_time.count()));
     }
   }
 
